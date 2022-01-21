6506fa75ce35fa510939be25801c9513eb0ac20a
spectre900 <nayakpratham0@gmail.com>
2022-01-22 00:13:56 +0530

    add GLB Load

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import hand from "./Hand/hand.glb";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+import React, { Component } from "react";
+
+class App extends Component {
+  componentDidMount() {
+    var scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xdddddd);
+    var camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+
+    var width = 100;
+    var height = 100;
+    var intensity = 1.4;
+    var rectLight = new THREE.RectAreaLight(0xffffff, intensity, width, height);
+    rectLight.position.set(1, 1, 10);
+    rectLight.lookAt(1, 1, 3);
+    scene.add(rectLight);
+
+    let renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document
+      .getElementsByClassName("three-canvas")[0]
+      .appendChild(renderer.domElement);
+
+    camera.position.z = 5;
+    var animate = function () {
+      requestAnimationFrame(animate);
+      renderer.render(scene, camera);
+    };
+
+    let loader = new GLTFLoader();
+    loader.load(
+      hand,
+      (gltf) => {
+        scene.add(gltf.scene);
+        console.log(gltf.scene);
+      },
+      (xhr) => {
+        console.log(xhr);
+      }
+    );
+
+    animate();
+  }
+  render() {
+    return <div className="three-canvas" />;
+  }
 }
 
 export default App;
