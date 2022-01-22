b3a8319e34b6720196ea6f26aeeb1ee0bd174a98
spectre900 <nayakpratham0@gmail.com>
2022-01-22 23:04:21 +0530

    first animation

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,28 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import React, { Component } from "react";
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       
+    };
+
+    this.start = null;
+    this.hand = null;
+    this.scene = null;
+    this.camera = null;
+    this.renderer = null;
+  }
+  
+  
   componentDidMount() {
-    var scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xdddddd);
-    var camera = new THREE.PerspectiveCamera(
+
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0xdddddd);
+
+    this.camera = new THREE.PerspectiveCamera(
       75,
       window.innerWidth / window.innerHeight,
       0.1,
@@ -22,44 +40,61 @@ class App extends Component {
     var rectLight = new THREE.RectAreaLight(0xffffff, intensity, width, height);
     rectLight.position.set(1, 1, 10);
     rectLight.lookAt(1, 1, 3);
-    scene.add(rectLight);
 
-    let renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document
-      .getElementsByClassName("three-canvas")[0]
-      .appendChild(renderer.domElement);
+    this.scene.add(rectLight);
 
-    camera.position.z = 5;
-    camera.position.y = 1;
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.getElementsByClassName("canvas")[0].appendChild(this.renderer.domElement);
 
-    var bone;
+    this.camera.position.z = 5;
+    this.camera.position.y = 1;
 
     let loader = new GLTFLoader();
     loader.load(
       hand,
       (gltf) => {
-        bone = gltf.scene.getObjectByName("Bone");
-        scene.add(gltf.scene);
+        this.hand = gltf.scene;
+        this.scene.add(this.hand);
       },
       (xhr) => {
         console.log(xhr);
       }
     );
 
-    var animate = function(){
-      requestAnimationFrame(animate);
-      if(bone){
-        bone.rotation.x+=0.02;
-      }
-      renderer.render(scene, camera);
-    }
-
-    animate();
-
   }
+
+  animate = (boneName, rotateAxis, rotateAngle) => {
+    if(this.hand.getObjectByName(boneName).rotation[rotateAxis] < rotateAngle){
+      requestAnimationFrame(() =>{
+        this.animate(boneName, rotateAxis, rotateAngle);
+      });
+      this.hand.getObjectByName(boneName).rotation[rotateAxis] += 0.02;
+      this.renderer.render(this.scene, this.camera);
+    }
+  }
+
+  showMF = () => {
+    requestAnimationFrame(() => {
+      this.animate("Bone", "y", Math.PI);
+    });
+    requestAnimationFrame(() => {
+      this.animate("Bone008", "z", Math.PI/2.1);
+    });
+    requestAnimationFrame(() => {
+      this.animate("Bone009", "z", Math.PI/2);
+    });
+  }
+
   render() {
-    return <div className="three-canvas" />;
+    return (
+      <div>
+        <div className="canvas" />
+        <button onClick={this.showMF}>
+          CLICK ME
+        </button>
+      </div>
+    )
   }
 }
 
